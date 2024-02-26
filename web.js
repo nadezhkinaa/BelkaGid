bb2b0d6ec3c827a36ec4bf0273ee12dcc02389a4
nadezhkina <ann.nadezhkina@mail.ru>
2024-02-26 21:33:04 +0300

    initial commit

---
+++ web.js
@@ -0,0 +1,13 @@
+function displayTime() {
+    let element = document.getElementById("clock"); // Найти элемент с id="clock"
+    let now = new Date();    // Получить текущее время
+    element.innerHTML = now.toLocaleTimeString(); // Отобразить время
+    setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
+}
+window.onload = displayTime; // Начать отображение времени после загрузки документа
+
+
+document.getElementById('add-form').addEventListener('submit',function(event){
+event.preventDefault()
+alert('sdf')
+})
