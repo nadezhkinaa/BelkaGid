e1478bd9ff451f53589c44db9649fdb6fc3bbab6
Michael <mishumpel@mail.ru>
2024-02-26 22:11:28 +0300

    add sample validation

--- web.js
+++ web.js
@@ -1,13 +1,19 @@
-function displayTime() {
-    let element = document.getElementById("clock"); // Найти элемент с id="clock"
-    let now = new Date();    //  Получить текущее время
-    element.innerHTML = now.toLocaleTimeString(); // Отобразить время
-    setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
+function validate() {
+    var userlogin = document.forms["form"]["login"].value;
+    var userpassword = document.forms["form"]["password"].value;
+    
+// Проверяем, что логин и пароль не пустые
+    if (userlogin.trim() === '' || userpassword.trim() === '') {
+        alert("Поля заполни");
+        return false; // Отменяем отправку формы
+    }
+
+    // Проверка дополнительных условий, например, длины пароля
+    if (userpassword.length < 6) {
+        alert('Пароль должен содержать не менее 6 символов.');
+        return false; // Отменяем отправку формы
+    }
+
+    // Все проверки пройдены, форма валидна
+    return true;
 }
-window.onload = displayTime; // Начать отображение времени после загрузки документа
-
-
-document.getElementById('add-form').addEventListener('submit',function(event){
-event.preventDefault()
-alert('sdf')
-})
