333be208c368e197f4aad94b4d6a2c40ce945035
Michael <mishumpel@mail.ru>
2024-03-10 17:09:38 +0300

    lab2 version

--- vdform.js
+++ vdform.js
@@ -1,3 +1,5 @@
+var isValidPass = false;
+var isValidEmail = false;
 const form = document.querySelector('#form');
 const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 const checkValidity = (input) => {
@@ -8,10 +10,12 @@ const checkValidity = (input) => {
     if (input.type == "email") {
         if (EMAIL_REGEXP.test(input.value)) {
         input.classList.add('text-field__input_valid');
-   input.nextElementSibling.textContent = 'Отлично!';
+        input.nextElementSibling.textContent = 'Отлично!';
+        isValidEmail = true;
         }else{
         input.classList.add('text-field__input_invalid');
-   input.nextElementSibling.textContent = input.validationMessage;
+        input.nextElementSibling.textContent = input.validationMessage;
+        isValidEmail = false;
         }
     }
     // строчная + заглавная + цифра, от 6 символов
@@ -20,14 +24,14 @@ if (input.type == "password") {
     if (re.test(input.value)) {
         input.classList.add('text-field__input_valid');
    input.nextElementSibling.textContent = 'Отлично!';
+   isValidPass = true;
         }else{
         input.classList.add('text-field__input_invalid');
    input.nextElementSibling.textContent = "Пароль должен содержать цифру, заглавную и прописную латинские буквы. Не менее 6 символов.";
+       isValidPass = false;
         }
 }
 }
-
-
 
 
 const checkValidityAll = () => {
@@ -48,11 +52,14 @@ const onCheckValidity = (e) => {
 form.addEventListener('change', onCheckValidity);
 form.addEventListener('keydown', onCheckValidity);
 form.addEventListener('keyup', onCheckValidity);
-
 //checkValidityAll();
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkValidityAll();
+
+    if (isValidEmail && isValidPass){window.location.replace("lichnkabinet.html");}
+
+
 });
