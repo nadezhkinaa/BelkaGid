6298a0fc0ed6b0ddd900cb7f9b2e4e4bac6c35da
Mikhail <mishumpel@mail.ru>
2024-05-06 22:28:09 +0300

    added favouritesmodel

--- djangoProject/belka/admin.py
+++ djangoProject/belka/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from belka.models import Place, Cafe, Shop
+from belka.models import Place, Cafe, Shop, UserFavourites
 
 admin.site.register(Place)
 admin.site.register(Cafe)
 admin.site.register(Shop)
+admin.site.register(UserFavourites)
