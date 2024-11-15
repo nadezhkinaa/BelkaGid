ccfb4bacf77f36add212f8888b04cc012fb78e0f
Mikhail <mishumpel@mail.ru>
2024-11-15 19:32:37 +0300

    selenium primitive skeleton

--- djangoProject/belka/tests.py
+++ djangoProject/belka/tests.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from belka.forms import SignUpForm, CustomAuthenticationForm, FeedbackForm
-from belka.models import Place
-from django.test.client import Client
+from belka.models import Place, Route
 
 
 # some_path_to/virtualenv/lib/python3.11/site-packages/django/test/runner.py (probably at line 1044)
@@ -13,29 +13,44 @@ from django.test.client import Client
 # https://docs.djangoproject.com/en/dev/topics/testing/overview/
 # https://habr.com/ru/articles/122156/
 # https://yourtodo.ru/ru/posts/django-testirovanie/
+# https://sky.pro/media/kak-provodit-testirovanie-s-ispolzovaniem-selenium/
+
 def create_places():
-    Place.objects.create(name="Место 1", short_description="Краткое описание для места 1", image=None,
-                         description="Полное описание для места 1", rating=4.99, type=1, map_id=2,
-                         redirect_url="redir_url", map_x=12, map_y=21, geo_x=120, geo_y=123)
-    Place.objects.create(name="Place2", short_description="Краткое описание для места 2", image=None,
-                         description="Полное описание для места 2", rating=4.5, type=2, map_id=3,
-                         redirect_url="redir_url_2", map_x=15, map_y=25, geo_x=125, geo_y=128)
+    from django.db import models
+    Place.objects.create(image=models.FilePathField(), name="Место 1", short_description="Краткое описание для места 1",
+                         description="Полное описание для места 1", rating=4.99, type=1, map_id=1,
+                         redirect_url="places", map_x=100, map_y=350, geo_x=56.006621, geo_y=37.204354)
 
-    Place.objects.create(name="Место 3", short_description="Краткое описание для места 3", image=None,
-                         description="Полное описание для места 3", rating=3.8, type=3, map_id=4,
-                         redirect_url="redir_url_3", map_x=18, map_y=30, geo_x=130, geo_y=133)
+    Place.objects.create(image=models.FilePathField(), name="Place2", short_description="Краткое описание для места 2",
+                         description="Полное описание для места 2", rating=4.5, type=2, map_id=2,
+                         redirect_url="places", map_x=120, map_y=-10, geo_x=55.981894, geo_y=37.213759)
 
-    Place.objects.create(name="Место 4", short_description="Краткое описание для места 4", image=None,
-                         description="Полное описание для места 4", rating=4.2, type=1, map_id=5,
-                         redirect_url="redir_url_4", map_x=21, map_y=35, geo_x=135, geo_y=138)
+    Place.objects.create(image=models.FilePathField(), name="Место 3", short_description="Краткое описание для места 3",
+                         description="Полное описание для места 3", rating=3.8, type=3, map_id=3,
+                         redirect_url="places", map_x=-400, map_y=-100, geo_x=55.97926, geo_y=37.151972)
 
-    Place.objects.create(name="Место 5", short_description="Краткое описание для места 5", image=None,
-                         description="Полное описание для места 5", rating=4.9, type=2, map_id=6,
-                         redirect_url="redir_url_5", map_x=24, map_y=40, geo_x=140, geo_y=143)
+    Place.objects.create(image=models.FilePathField(), name="Место 4", short_description="Краткое описание для места 4",
+                         description="Полное описание для места 4", rating=4.2, type=1, map_id=4,
+                         redirect_url="places", map_x=0, map_y=30, geo_x=55.983229, geo_y=37.208158)
 
-    Place.objects.create(name="Место 6", short_description="Краткое описание для места 6", image=None,
-                         description="Полное описание для места 6", rating=3.5, type=3, map_id=7,
-                         redirect_url="redir_url_6", map_x=27, map_y=45, geo_x=145, geo_y=148)
+    Place.objects.create(image=models.FilePathField(), name="Место 5", short_description="Краткое описание для места 5",
+                         description="Полное описание для места 5", rating=4.9, type=2, map_id=5,
+                         redirect_url="places", map_x=350, map_y=180, geo_x=55.995364, geo_y=37.243641)
+
+    Place.objects.create(image=models.FilePathField(), name="Место 6", short_description="Краткое описание для места 6",
+                         description="Полное описание для места 6", rating=3.5, type=3, map_id=6,
+                         redirect_url="places", map_x=20, map_y=480, geo_x=56.014953, geo_y=37.208202)
+
+
+def create_routes():
+    Route.objects.create(name="Маршрут 1", short_description="short_description", rating=5.0, votes=15, creator=0,
+                         marshrut="1$2$3$4$5")
+    Route.objects.create(name="Маршрут 2", short_description="short_description", rating=5.0, votes=15, creator=0,
+                         marshrut="1$2$3$4$5")
+    Route.objects.create(name="Маршрут 3", short_description="short_description", rating=5.0, votes=15, creator=0,
+                         marshrut="1$2$3$4$5")
+    Route.objects.create(name="Маршрут 4", short_description="short_description", rating=5.0, votes=15, creator=0,
+                         marshrut="1$2$3$4$5")
 
 
 class PlaceTestCase(TestCase):
@@ -246,3 +261,42 @@ class ViewTest(TestCase):
         response = self.client.get("/register/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'register.html')
+
+
+class TestSelenium(StaticLiveServerTestCase):
+    """
+    Class for testing frontend with Selenium
+    """
+
+    def setUp(self):
+        User.objects.create_user(username='username', password='Pas$w0rd')
+
+        self.browser = webdriver.Chrome()
+        create_places()
+        create_routes()
+
+    def login(self):
+        self.browser.get(self.live_server_url + "/login/")
+
+        login_field = self.browser.find_element(By.NAME, "username")
+        password_field = self.browser.find_element(By.NAME, "password")
+        login_button = self.browser.find_element(By.CLASS_NAME, "buttonvhod")
+
+        login_field.send_keys('username')
+        password_field.send_keys('Pas$w0rd')
+        login_button.click()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_correct_title(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn("БелкаГид", self.browser.title)
+
+    def test_route_items(self):
+        self.login()
+        popular_1 = self.browser.find_element(By.ID, "popular1")
+        popular_1.click()
+
+        self.assertIn("БелкаГид", self.browser.title)
+        self.assertEqual("Маршрут 2", self.browser.find_element(By.ID, 'rName').text)
