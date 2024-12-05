d478f22ce317cfca0437376ec638fd049b41344f
Mikhail <mishumpel@mail.ru>
2024-12-05 16:01:07 +0300

    locust testing

---
+++ djangoProject/belka/tests/locust_testing.py
@@ -0,0 +1,16 @@
+# locust -f '.\belka\tests\locust_testing.py'
+
+
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def index(self):
+        self.client.get("")
+
+    @task
+    def about(self):
+        self.client.get("about")
