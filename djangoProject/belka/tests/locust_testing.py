0cabe9514a69595c53a029aec789f084e35a1df7
Mikhail <mishumpel@mail.ru>
2024-12-05 17:53:34 +0300

    locust

--- djangoProject/belka/tests/locust_testing.py
+++ djangoProject/belka/tests/locust_testing.py
@@ -5,10 +5,12 @@ from locust import HttpUser, task, between
 
 TEST_LOGIN = ""
 TEST_PASSWORD = ""
+WAIT_FROM = 0.5
+WAIT_TO = 2
 
 
-class BelkaUser(HttpUser):
-    wait_time = between(1, 5)
+class BelkaLoads(HttpUser):
+    wait_time = between(WAIT_FROM, WAIT_TO)
 
     @task
     def index(self):
@@ -17,6 +19,66 @@ class BelkaUser(HttpUser):
     @task
     def about(self):
         self.client.get("/about")
+
+    @task
+    def admin(self):
+        self.client.get("/admin")
+
+    @task
+    def places_page(self):
+        self.client.get("/places")
+
+    @task
+    def cafe_page(self):
+        self.client.get("/cafe")
+
+    @task
+    def shop_page(self):
+        self.client.get("/shop")
+
+    @task
+    def profile_redirect_page(self):
+        self.client.get("/profile")
+
+    @task
+    def profile_page(self):
+        self.client.get("/profile/info")
+
+    @task
+    def profile_routes_page(self):
+        self.client.get("/profile/routes")
+
+    @task
+    def profile_orders_page(self):
+        self.client.get("/profile/orders")
+
+    @task
+    def create_order_page(self):
+        self.client.get("/profile/orders/create")
+
+    @task
+    def login(self):
+        self.client.get("/login")
+
+    @task
+    def log_out(self):
+        self.client.get("/logout")
+
+    @task
+    def signup(self):
+        self.client.get("/register")
+
+    @task
+    def message_sent(self):
+        self.client.get("/message_sent")
+
+    @task
+    def events(self):
+        self.client.get("/events")
+
+
+class BelkaForms(HttpUser):
+    wait_time = between(WAIT_FROM, WAIT_TO)
 
     @task
     def login(self):
@@ -33,3 +95,20 @@ class BelkaUser(HttpUser):
         client.post(URL, data={'csrfmiddlewaretoken': csrftoken,
                                'username': TEST_LOGIN,
                                'password': TEST_PASSWORD})
+
+    @task
+    def send_message(self):
+        URL = "http://127.0.0.1:8000/about/"
+        client = self.client
+        client.get(URL)
+        if 'csrftoken' in client.cookies:
+            # Django 1.6 and up
+            csrftoken = client.cookies['csrftoken']
+        else:
+            # older versions
+            csrftoken = client.cookies['csrf']
+
+        client.post(URL, data={'csrfmiddlewaretoken': csrftoken,
+                               'name': "testlogin",
+                               'email': "testmail@mail.ru",
+                               'message': "test message"})
